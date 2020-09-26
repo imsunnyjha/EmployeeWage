158ce075c486659e84a182f9985cdb0b8e7ef07b
Sunny Jha <sunnyjha11897@gmail.com>
2020-09-26 23:36:15 +0530

    [SUNNY] Add. Added UseCase5 for Monthly Wage Calculation

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -8,6 +8,7 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NO_OF_WORKING_DAYS = 20;
 
         static void Main(string[] args)
         {
@@ -16,35 +17,41 @@ namespace EmployeeWage
             //Variables
             int empHours = 0;
             int empWage = 0;
+            int monthlyWage = 0;
 
-            Random random = new Random();
-
-            //Computation
-            int empCheck = random.Next(0, 3);
-
-            //Check Employee Attendance and WorkHours using Switch Case
-            switch (empCheck)
+            for (int day = 0; day < NO_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHours = 8;
-                    Console.WriteLine("Employee is Present");
-                    break;
+                Random random = new Random();
 
-                case IS_PART_TIME:
-                    empHours = 4;
-                    Console.WriteLine("Employee is Present");
-                    break;
+                //Computation
+                int empCheck = random.Next(0, 3);
 
-                default:
-                    empHours = 0;
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                //Check Employee Attendance and WorkHours using Switch Case
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHours = 8;
+                        Console.WriteLine("Full Time Employee is Present");
+                        break;
+
+                    case IS_PART_TIME:
+                        empHours = 4;
+                        Console.WriteLine("Part Time Employee is Present");
+                        break;
+
+                    default:
+                        empHours = 0;
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+
+                //DailyEmployeeWage
+
+                empWage = empHours * EMP_RATE_PER_HOUR;
+                monthlyWage += empWage;
+                Console.WriteLine("Emp Wage: " + empWage);
             }
-
-            //DailyEmployeeWage
-           
-            empWage = empHours * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Total Monthly Wage: " + monthlyWage);
         }
     }
 }
