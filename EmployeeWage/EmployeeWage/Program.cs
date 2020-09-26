3cea095926040b3749b0393df55e905ff2c7c79b
Sunny Jha <sunnyjha11897@gmail.com>
2020-09-26 23:14:08 +0530

    [SUNNY] Add. Added UseCase4 for Switch Case Statement

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -4,14 +4,14 @@ namespace EmployeeWage
 {
     class Program
     {
+        //Constants
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Employee Wage Computation Problem!");
-
-            //Constants
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
 
             //Variables
             int empHours = 0;
@@ -22,22 +22,23 @@ namespace EmployeeWage
             //Computation
             int empCheck = random.Next(0, 3);
 
-            //Check Employee Attendance
-            if (empCheck == IS_FULL_TIME)
+            //Check Employee Attendance and WorkHours using Switch Case
+            switch (empCheck)
             {
-                empHours = 8;
-                Console.WriteLine("Employee is Present");
-            }
+                case IS_FULL_TIME:
+                    empHours = 8;
+                    Console.WriteLine("Employee is Present");
+                    break;
 
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHours = 4;
-                Console.WriteLine("Employee is Present");
-            }
-            else
-            {
-                empHours = 0;
-                Console.WriteLine("Employee is Absent");
+                case IS_PART_TIME:
+                    empHours = 4;
+                    Console.WriteLine("Employee is Present");
+                    break;
+
+                default:
+                    empHours = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
 
             //DailyEmployeeWage
