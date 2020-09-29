5819069b65b2da558b8a956e2bd2ccc62885cb78
Sunny Jha <sunnyjha11897@gmail.com>
2020-09-29 12:57:29 +0530

    [SUNNY] Refactor. UseCase10 for Managing Wage of Multiple Companies

---
+++ EmployeeWage/EmployeeWage/Wage/CompanyWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeWage.Company;
+
+namespace EmployeeWage.Wage
+{
+    class CompanyWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int noOfWorkingDays;
+        public int maxMonthlyHours;
+        public int totalEmpWage;
+
+        public CompanyWage(string company, int empRatePerHour, int noOfWorkingDays, int maxMonthlyHours)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.noOfWorkingDays = noOfWorkingDays;
+            this.maxMonthlyHours = maxMonthlyHours;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for Company " + this.company + " is:" + this.totalEmpWage;
+        }
+    }
+}
